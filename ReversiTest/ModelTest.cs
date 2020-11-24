5fc93891d50c996b8f98518d937ecb955baf8290
Botond Pilipar <botondpilipar@github.com>
2020-11-24 16:06:57 +0100

    Integrate unit test to project

---
+++ ReversiTest/ModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kd417d.eva;
+
+namespace kd417d.eva.reversi
+{
+    [TestClass]
+    class ModelTest
+    {
+        private ReversiTable _table;
+        [TestInitialize]
+        public void Setup()
+        {
+            _table = new ReversiTable(10, 10);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ ReversiTest/TableLogicTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kd417d.eva;
+
+namespace kd417d.eva.reversi
+{
+    [TestClass]
+    public class TableLogicTest
+    {
+        private TableLogic _logic;
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+    }
+}
