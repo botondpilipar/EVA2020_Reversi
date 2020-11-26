bb51c0e831486b19b706e865d88b484e139ba6c4
Botond Pilipar <botondpilipar@github.com>
2020-11-26 19:36:00 +0100

    Add test utilities
    Implement EventSpy similar to Qt version.
    Add Moq to project dependencies.

---
+++ ReversiTest/utility/EventSpy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kd417d.eva.test.utility
+{
+    class EventSpy<EventArgT>
+    {
+        public List<EventArgT> CapturedArguments { get; }
+
+        public EventSpy()
+        {
+            CapturedArguments = new List<EventArgT>();
+        }
+        public void OnEventRaised(object sender, EventArgT arg)
+        {
+            CapturedArguments.Add(arg);
+        }
+        public bool IsEventRaised()
+        {
+            return CapturedArguments.Count != 0;
+        }
+        public bool IsEmpty()
+        {
+            return CapturedArguments.Count == 0;
+        }
+    }
+}
